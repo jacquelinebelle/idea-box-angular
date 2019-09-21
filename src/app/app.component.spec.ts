e21b0a5bebbb769106ee1acbdbeb4b98626f2f3b
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-09-21 13:21:18 -0600

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'idea-box-angular'`, () => {
+  it(`should have as title 'Idea Box'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('idea-box-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('idea-box-angular app is running!');
+    expect(app.title).toEqual('Idea Box');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'idea-box-angular';
+  title = 'Idea Box';
 }
